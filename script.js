3f95ca509b9fcd5e024d90d507f46ebe892791ed
ArleyBT <abt.2709@gmail.com>
2023-11-09 22:35:07 -0500

    Formulario

--- script.js
+++ script.js
@@ -1,31 +1,41 @@
 class ValidadorFormulario {
-    constructor(formId){
-        this.form = document.getElementById(formId);
+    constructor(IdFormulario){
+        this.form = document.getElementById(IdFormulario);
         // Seleccionar inputs
         this.username = document.getElementById('username');
-        this.emailImput = document.getElementById('email');
-        this.floatingTextarea = document.getElementById('Deja tu mensaje');
+        this.email = document.getElementById('email');
+        this.mensaje = document.getElementById('Deja tu mensaje');
 
         //Seleccionar span de error
         this.usernameError = document.getElementById('usernameError');
-        this.emailImputError = document.getElementById('emailError');
-        this.floatingTextareaError = document.getElementById('Deja tu mensajeError');
+        this.emailError = document.getElementById('emailError');
+        this.mensajeError = document.getElementById('Deja tu mensajeError');
     
         //Escuchar eventos del usuario
-        this.form.addEventListener('submit', (Event) =>{
-            Event.preventDefault(); //Evitar el envio de formulario 
+        this.form.addEventListener('submit', (event) =>{
+            event.preventDefault(); //Evitar el envio de formulario 
 
             if(this.ValidarFormulario()) {
                 alert('Enviando informacion')
             }
 
         })
-    }
-
+    }  
+    
     ValidarFormulario(){
-        console.log('Inglreso al metodo')
+        let valid = true;
+    
+        if (this.username.value === '') {
+            this.usernameError.textContent = 'EL nombre del usuarios es obligatorio'
+            this.username.classList.add('error-imput')
+            valid = false;
+        } else {
+            this.usernameError.textContent = '';
+            this.username.classList.remove('error-input');
+        } 
     }
 
 }
+
 
 
